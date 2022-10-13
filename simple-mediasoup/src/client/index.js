638e325eba64eb7bb490f2efdc54ddf11741c1df
odini <odini@nhnsoft.com>
2022-10-13 09:06:09 +0900

    simple example

---
+++ simple-mediasoup/src/client/index.js
@@ -0,0 +1,359 @@
+import mediasoup from "mediasoup-client";
+import { uuidv4, request, sleep, log, err } from "./utils";
+import { CAM_VIDEO_SIMULCAST_ENCODINGS } from "./constant";
+
+class Client {
+  constructor() {
+    this.state = {
+      myPeerId: uuidv4(),
+      device: new mediasoup.Device(),
+      localCam: null,
+      joined: false,
+      sendTransport: null,
+      camVideoProducer: null,
+      pollingInterval: null,
+      peers: {},
+      isReceiver: false,
+      consumers: [],
+    };
+  }
+
+  /**
+   * @title 방 입장
+   * @description 회의를 제어한다.
+   * @returns
+   */
+  async joinRoom() {
+    const { joined, myPeerId, device } = this.state;
+    // 방에 입장된 상태는 더 이상 진행하지 않음
+    if (joined) {
+      return;
+    }
+
+    try {
+      // http 요청 - 새로운 피어임을 알린다.
+      const res = await request({
+        endpoint: "join-as-new-peer",
+        method: "post",
+        payload: {
+          peerId: myPeerId,
+        },
+      });
+      const { routerRtpCapabilities } = res;
+      // mediasoup-client device가 로드되지 않은 경우(처음 연결)
+      if (!device.loaded) {
+        // 디바이스를 초기화(로드) 한다.
+        await device.load({ routerRtpCapabilities });
+      }
+      // 방 입장 상태로 변경
+      this.state.joined = true;
+    } catch (e) {
+      console.error(e);
+      return;
+    }
+
+    // 1초 간격 폴링(인터벌)
+    this.state.pollingInterval = setInterval(async () => {
+      // 폴링, 업데이트 로직
+      const { error } = await this.pollAndUpdate();
+      // 에러가 있다면 오류
+      if (error) {
+        // 인터벌 종료
+        clearInterval(this.state.pollingInterval);
+        // 디버그 - 로깅
+        err(error);
+      }
+    }, 1000);
+  }
+  /**
+   * @title 폴링, 업데이트 로직
+   * @description 활성화된 스피커 갱신, 프로듀서 (비디오, 화면) 통계 표시 갱신, 컨슈머 통계 표시 갱신
+   * @returns
+   */
+  async pollAndUpdate() {
+    const { myPeerId } = this.state;
+    // http 요청
+    const res = await request({
+      endpoint: "sync",
+      method: "post",
+      payload: {
+        peerId: myPeerId,
+      },
+    });
+    const { peers, error } = res;
+
+    this.state.peers = peers;
+
+    if (this.state.isReceiver) {
+      document.querySelector("#tracks").innerHTML = Object.keys(peers)
+        .filter((key) => {
+          return key !== myPeerId;
+        })
+        .map((key) => {
+          return `<button data-peer-id="${key}" data-media-tag="${
+            Object.entries(peers[key].media)[0]?.[0]
+          }">subscribe</button>`;
+        })
+        .join("");
+    }
+
+    if (error) {
+      return { error };
+    }
+
+    return {};
+  }
+  /**
+   * @title 카메라 스트림 전송
+   */
+  async sendCameraStreams() {
+    /**
+     * 방에 참여하고 카메라가 시작되었는지 확인한다.
+     * 이미 호출된 함수는 아무것도 하지 않는다.
+     */
+    // 방 입장
+    await this.joinRoom();
+    // 카메라 시작
+    await this.startCamera();
+
+    if (!this.state.sendTransport) {
+      this.state.sendTransport = await this.createTransport("send");
+    }
+
+    /*
+    비디오 전송을 시작한다.
+    전송 로직은 카메라 비디오 트랙에 대한 아웃바운드 rtp 스트림을 설정하기 위해 서버와 신호 대화를 시작한다.
+    createTransport() 함수에는 UI의 체크박스가 체크가 안된 경우 일시 중지된 상태에서 스트림을 시작하도록 서버에 요청하는 로직이 포함되어 있다.
+    따라서 클라이언트 측 camVideoProducer 개체가 있다면 적절하게 일시 중지되도록 설정되어야 한다.
+  */
+    this.state.camVideoProducer = await this.state.sendTransport.produce({
+      track: this.state.localCam.getVideoTracks()[0],
+      encodings: CAM_VIDEO_SIMULCAST_ENCODINGS,
+      appData: { mediaTag: "cam-video" },
+    });
+  }
+
+  /**
+   * @title 카메라 시작
+   * @returns
+   */
+  async startCamera() {
+    if (this.state.localCam) {
+      return;
+    }
+    try {
+      this.state.localCam = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+    } catch (e) {
+      console.error("start camera error", e);
+    }
+  }
+
+  /**
+   * @title transport 생성
+   * @description transport를 생성하고, 전송 방향에 맞는 signal 로직을 연결한다.
+   * @param {*} direction
+   * @returns
+   */
+  async createTransport(direction) {
+    const { myPeerId, device } = this.state;
+    /**
+     * 서버에 서버 측 transport 객체를 생성하도록 요청하고
+     * 클라이언트 측 transport를 생성하는 데 필요한 정보를 다시 보내야한다.
+     */
+    let transport;
+    // http 요청
+    const { transportOptions } = await request({
+      endpoint: "create-transport",
+      method: "post",
+      payload: { peerId: myPeerId, direction },
+    });
+
+    if (direction === "recv") {
+      transport = await device.createRecvTransport(transportOptions);
+    } else if (direction === "send") {
+      transport = await device.createSendTransport(transportOptions);
+    } else {
+      throw new Error(`bad transport 'direction': ${direction}`);
+    }
+
+    /**
+     * mediasoup-client는 미디어가 처음으로 흐르기 시작해야 연결 이벤트를 보낸다.
+     * dtlsParameters를 서버로 보낸 다음 성공하면 callback()을 호출하고 실패하면 errback()을 호출한다.
+     */
+    transport.on("connect", async ({ dtlsParameters }, callback, errback) => {
+      log("transport connect event", direction);
+      // http 요청
+      const { error } = await request({
+        endpoint: "connect-transport",
+        method: "post",
+        payload: {
+          peerId: myPeerId,
+          transportId: transportOptions.id,
+          dtlsParameters,
+        },
+      });
+      if (error) {
+        err("error connecting transport", direction, error);
+        errback();
+        return;
+      }
+      callback();
+    });
+
+    if (direction === "send") {
+      /**
+       * transport 전송은 전송을 시작하기 위해 새 track을 설정해야 할 때 생성 이벤트를 내보낸다.
+       * producer의 appData는 매개변수로 전달
+       */
+      transport.on(
+        "produce",
+        async ({ kind, rtpParameters, appData }, callback, errback) => {
+          log("transport produce event", appData.mediaTag);
+          /**
+           * 서버 측 producer 객체를 설정하기 위해 서버에게 우리의 정보를 전달하고 생산자 ID를 반환 받는다.
+           * 성공 시 callback() 또는 호출 실패 시 errback()  호출
+           */
+          // http 요청
+          const { error, id } = await request({
+            endpoint: "send-track",
+            method: "post",
+            payload: {
+              peerId: myPeerId,
+              transportId: transportOptions.id,
+              kind,
+              rtpParameters,
+              paused: false,
+              appData,
+            },
+          });
+          if (error) {
+            err("error setting up server-side producer", error);
+            errback();
+            return;
+          }
+          callback({ id });
+        }
+      );
+    }
+
+    return transport;
+  }
+
+  async showTracks() {
+    this.state.isReceiver = true;
+    await this.joinRoom();
+    document.querySelector("#tracks").addEventListener("click", (e) => {
+      const { peerId, mediaTag } = e.target.dataset;
+      this.subscribeToTrack(peerId, mediaTag);
+    });
+  }
+
+  /**
+   * @title track 구독
+   * @param {string} peerId
+   * @param {*} mediaTag
+   * @returns
+   */
+  async subscribeToTrack(peerId, mediaTag) {
+    log("subscribe to track", peerId, mediaTag);
+
+    // receive transport 를 갖고 있지 않다면 receive transport를 생성한다.
+    if (!this.state.recvTransport) {
+      this.state.recvTransport = await this.createTransport("recv");
+    }
+    const { myPeerId, consumers, recvTransport, device } = this.state;
+
+    // track을 위한 컨슈머 검색
+    const hasConsumer = !!consumers.find((c) => {
+      return c.appData.peerId === peerId && c.appData.mediaTag === mediaTag;
+    });
+
+    if (hasConsumer) {
+      // consumer 존재한다면, 호출되지 않아야 하므로 리턴처리
+      err("already have consumer for track", peerId, mediaTag);
+      return;
+    }
+
+    /**
+     * http 요청 - 서버에 서버 측 consumer 객체를 만들고 전송하도록 요청하고
+     * 클라이언트 측 consumer를 만드는 데 필요한 정보를 백업한다.
+     */
+    const consumerParameters = await request({
+      endpoint: "recv-track",
+      payload: {
+        peerId: myPeerId,
+        mediaTag,
+        mediaPeerId: peerId,
+        rtpCapabilities: device.rtpCapabilities,
+      },
+    });
+    log("consumer parameters", consumerParameters);
+    const consumer = await recvTransport.consume({
+      ...consumerParameters,
+      appData: { peerId, mediaTag },
+    });
+
+    log("created new consumer", consumer.id);
+
+    /**
+     * 서버 측 consumer는 일시 중지된 상태에서 시작된다.
+     * 연결될 때까지 기다린 다음 첫 번째 키프레임을 가져오고 비디오 표시를 시작하기 위해 서버 resume 요청을 보낸다.
+     */
+    while (recvTransport.connectionState !== "connected") {
+      log("  transport connstate", recvTransport.connectionState);
+      await sleep(100);
+    }
+    // 클라이언트 준비 완료, peer에 미디어를 보내달라고 요청한다.
+    await request({
+      endpoint: "resume-consumer",
+      method: "post",
+      payload: { peerId: myPeerId, consumerId: consumer.id },
+    });
+    await consumer.resume();
+
+    // consumer 목록 추가
+    consumers.push(consumer);
+
+    await this.addVideoAudio(consumer);
+  }
+
+  /**
+   * @title 비디오 또는 오디오 추가
+   * @param {*} consumer
+   * @returns
+   */
+  addVideoAudio(consumer) {
+    if (!(consumer && consumer.track)) {
+      return;
+    }
+    const el = document.createElement(consumer.kind);
+    /**
+     * 오디오와 비디오 엘리먼트를 만들기 위해서 일부 어트리뷰트를 설정한다.
+     * 오디오를 재생하려면 mic/camera에서 캡처해야 한다.
+     */
+    if (consumer.kind === "video") {
+      el.setAttribute("playsinline", true);
+    } else {
+      el.setAttribute("playsinline", true);
+      el.setAttribute("autoplay", true);
+    }
+    document.querySelector("#video").appendChild(el);
+    el.srcObject = new MediaStream([consumer.track.clone()]);
+    el.consumer = consumer;
+    /**
+     * play의 성공을 기다리기보다 play 하기 전에 yield 하고 리턴한다.
+     * play()는 producer 일시 중지를 해제할 때까지
+     * producer 일시 중지 트랙에서 성공하지 못한다.
+     */
+    el.play()
+      .then(() => {})
+      .catch((e) => {
+        err(e);
+      });
+  }
+}
+
+export default new Client();
